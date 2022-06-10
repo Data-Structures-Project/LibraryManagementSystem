d89e3ce16afe097b8e1768b122d0f22597f3b164
mstfmrt <mstf01ua@gmail.com>
2022-06-10 21:22:17 +0300

    update Main

--- src/librarymanagementsystem/Main.java
+++ src/librarymanagementsystem/Main.java
@@ -1,7 +1,9 @@
 package librarymanagementsystem;
 
 public class Main {
-    public static void main(String [] args){
+
+
+    public static void main(String[] args) {
         public static final String ANSI_RED = "\u001B[31m";
 
         public static final String ANSI_GREEN = "\u001B[32m";
@@ -11,8 +13,8 @@ public class Main {
         public static final String ANSI_PURPLE = "\u001B[35m";
         public static final String ANSI_CYAN = "\u001B[36m";
 
-        public static void main(String [] args){
-            Scanner sc= new Scanner(System.in);
+        public static void main (String[]args){
+            Scanner sc = new Scanner(System.in);
 
             System.out.println(ANSI_BLUE + "====== Welcome to Library Management System ======");
             System.out.println(ANSI_CYAN + "1. Login");
@@ -21,10 +23,9 @@ public class Main {
             System.out.print(ANSI_GREEN + "Choose one of the options : ");
 
 
-
             String input = sc.next();
 
-            switch (input){
+            switch (input) {
                 case "1":
                     loginMenu();
                     break;
@@ -43,27 +44,24 @@ public class Main {
         }
 
 
-        static void loginMenu(){
+        static void loginMenu () {
             System.out.print(ANSI_GREEN + "Username :  ");
             Scanner sc = new Scanner(System.in);
-            String userName  = sc.next();
+            String userName = sc.next();
             System.out.print(ANSI_GREEN + "Password :  ");
-            String passwd  = sc.next();
+            String passwd = sc.next();
 
             //switch login func
             //search user func returns user type, user name
 
 
+        }
 
-
+        static void registerMenu () {
 
         }
 
-        static void registerMenu(){
-
-        }
-
-        static void administratorMenu(String usersName){
+        static void administratorMenu (String usersName){
             System.out.println(ANSI_BLUE + "\n\nHello " + usersName);
             System.out.println(ANSI_CYAN + "1. Manage Libraries");
             System.out.println(ANSI_CYAN + "2. Manege Library Managers");
@@ -73,7 +71,7 @@ public class Main {
             Scanner sc = new Scanner(System.in);
             String input = sc.next();
 
-            switch (input){
+            switch (input) {
                 case "1":
                     manageLibraries('a');
                     break;
@@ -89,7 +87,7 @@ public class Main {
             }
 
         }
-        static void libraryManagerMenu(String usersName){
+        static void libraryManagerMenu (String usersName){
             System.out.println(ANSI_BLUE + "\n\nHello " + usersName);
             System.out.println(ANSI_CYAN + "1. Edit Libraries");
             System.out.println(ANSI_CYAN + "2. Manege Librarians");
@@ -99,7 +97,7 @@ public class Main {
             Scanner sc = new Scanner(System.in);
             String input = sc.next();
 
-            switch (input){
+            switch (input) {
                 case "1":
                     manageLibraries('l');
                     break;
@@ -114,7 +112,7 @@ public class Main {
 
             }
         }
-        static void librarianMenu(String usersName){
+        static void librarianMenu (String usersName){
             System.out.println(ANSI_BLUE + "\n\nHello " + usersName);
             System.out.println(ANSI_CYAN + "1. Manage Readers");
             System.out.println(ANSI_CYAN + "2. Manege loan books");
@@ -127,7 +125,7 @@ public class Main {
             Scanner sc = new Scanner(System.in);
             String input = sc.next();
 
-            switch (input){
+            switch (input) {
                 case "1":
                     manageReaders();
                     break;
@@ -137,6 +135,12 @@ public class Main {
                 case "3":
                     manageBooks();
                     break;
+                case "4":
+                    manageMagazines();
+                    break;
+                case "5":
+                    searchBooks();
+                    break;
                 case "0":
                     System.exit(0);
                     break;
@@ -145,36 +149,58 @@ public class Main {
 
             }
         }
-        static void readerMenu(){
+        static void readerMenu () {
+            System.out.println(ANSI_BLUE + "\n\nHello " + usersName);
+            System.out.println(ANSI_CYAN + "1. Search books");
+            System.out.println(ANSI_CYAN + "2. Rate a book");
+            System.out.print(ANSI_GREEN + "Choose one of the options : ");
 
+            Scanner sc = new Scanner(System.in);
+            String input = sc.next();
+
+            switch (input) {
+                case "1":
+                    searchBooks();
+                    break;
+                case "2":
+                    rateBook();
+                    break;
+                case "0":
+                    System.exit(0);
+                    break;
+                default:
+                    break;
+
+            }
+
+
+            static void manageLibraries ( char userType){
+
+            }
+            static void manageLibraryManagers () {
+
+            }
+            static void manageLibrarians () {
+
+            }
+            static void manageReaders () {
+
+            }
+            static void manageLoanBooks () {
+
+            }
+            static void manageBooks () {
+
+            }
+            static void manageMagazines () {
+
+            }
+            static void searchBooks () {
+
+            }
+            static void rateBooks () {
+
+            }
         }
-
-
-
-        static void manageLibraries(char userType){
-
-        }
-        static void manageLibraryManagers(){
-
-        }
-        static void manageLibrarians(){
-
-        }
-        static void manageReaders(){
-
-        }
-        static void manageLoanBooks(){
-
-        }
-        static void manageBooks(){
-
-        }
-        static void manageMagazines(){
-
-        }
-        static void filterBooks(){
-
-        }
-
     }
 }
