78390de65a47fb5c47bba8f53fad391bc0e80ffc
MSC <62522721+MSCahyir@users.noreply.github.com>
2022-06-11 16:34:56 +0300

    Update MainService.java

--- src/librarymanagementsystem/service/MainService.java
+++ src/librarymanagementsystem/service/MainService.java
@@ -46,19 +46,24 @@ public class MainService {
         Administrator admin2 = new Administrator(2, "MustafaAdmin", "Mert", "Mustafa52Admin", "1234", lib1);
         Administrator admin3 = new Administrator(3, "EmreAdmin", "Yılmaz", "Emre9180Admin", "1234", lib3);
 
-        Publisher publisher1 = new Publisher((long)1, "Is Bankasi", "Is Bankasi, klasik yayinlar");
-        Publisher publisher2 = new Publisher((long)2, "Yapikredi", "Yapikredi, klasik yayinlar");
-        Publisher publisher3 = new Publisher((long)3, "Can Yayinlari", "Can yayinlari, klasik yayinlar");
+        Publisher publisher1 = new Publisher((long) 1, "Is Bankasi", "Is Bankasi, klasik yayinlar");
+        Publisher publisher2 = new Publisher((long) 2, "Yapikredi", "Yapikredi, klasik yayinlar");
+        Publisher publisher3 = new Publisher((long) 3, "Can Yayinlari", "Can yayinlari, klasik yayinlar");
 
-        Author author1 = new Author((long)1, "JRR Tolkien", "Tolkien", "Born in Turkey");
-        Author author2 = new Author((long)1, "Lev Nicolovig", "Tolstoy", "Born in Russia");
-        Author author3 = new Author((long)1, "Stefan", "Zweig", "Born in Germany");
+        Author author1 = new Author((long) 1, "JRR Tolkien", "Tolkien", "Born in Turkey");
+        Author author2 = new Author((long) 1, "Lev Nicolovig", "Tolstoy", "Born in Russia");
+        Author author3 = new Author((long) 1, "Stefan", "Zweig", "Born in Germany");
 
-        Material material1 = new Material((long)1, MaterialType.BOOK, "Beyaz Diş", Category.MYSTERY, new Date(1997513), author1, publisher1, 10, Situation.LIBRARY, Location.A1, "New Info");
-        Material material2 = new Material((long)2, MaterialType.BOOK, "Beyaz Diş", Category.MYSTERY, new Date(1997513), author1, publisher1, 10, Situation.LIBRARY, Location.A1, "New Info");
-        Material material3 = new Material((long)3, MaterialType.BOOK, "Beyaz Diş", Category.MYSTERY, new Date(1997513), author1, publisher1, 10, Situation.LIBRARY, Location.A1, "New Info");
-        Material material4 = new Material((long)4, MaterialType.BOOK, "Beyaz Diş", Category.MYSTERY, new Date(1997513), author1, publisher1, 10, Situation.LIBRARY, Location.A1, "New Info");
-        Material material5 = new Material((long)5, MaterialType.BOOK, "Beyaz Diş", Category.MYSTERY, new Date(1997513), author1, publisher1, 10, Situation.LIBRARY, Location.A1, "New Info");
+        Material material1 = new Material((long) 1, MaterialType.BOOK, "Beyaz Diş", Category.MYSTERY, new Date(1997513),
+                author1, publisher1, 10, Situation.LIBRARY, Location.A1, "New Info");
+        Material material2 = new Material((long) 2, MaterialType.BOOK, "Beyaz Diş", Category.MYSTERY, new Date(1997513),
+                author1, publisher1, 10, Situation.LIBRARY, Location.A1, "New Info");
+        Material material3 = new Material((long) 3, MaterialType.BOOK, "Beyaz Diş", Category.MYSTERY, new Date(1997513),
+                author1, publisher1, 10, Situation.LIBRARY, Location.A1, "New Info");
+        Material material4 = new Material((long) 4, MaterialType.BOOK, "Beyaz Diş", Category.MYSTERY, new Date(1997513),
+                author1, publisher1, 10, Situation.LIBRARY, Location.A1, "New Info");
+        Material material5 = new Material((long) 5, MaterialType.BOOK, "Beyaz Diş", Category.MYSTERY, new Date(1997513),
+                author1, publisher1, 10, Situation.LIBRARY, Location.A1, "New Info");
 
     }
 
@@ -73,14 +78,28 @@ public class MainService {
         return null;
     }
 
-    //TODO Register olurken kütüphane seçtirme
-    //TODO Longları düzenleme 
-    //TODO Id otomatik sectirme  
-    public static boolean register(String username, String password) {
+    // TODO Register olurken kütüphane seçtirme
+    // TODO Longları düzenleme
+    // TODO Id otomatik sectirme
+    public static boolean register(String name, String surname, String username, String password, Long libraryId) {
         if (accounts.viewInfo(username) == null) {
-            Library tempLib = new Library((long) 10, "sefaLib", City.ANKARA);
-            User tempNewAccount = new User(10, "sefa", "cahyir", username, password, tempLib);
-            accounts.create(tempNewAccount);
+            accounts.create(new User(10, name, surname, username, password, libraries.findById(libraryId)));
+            return true;
+        } else
+            return false;
+    }
+
+    public static boolean addLibraryManager(String name, String surname, String username, String password, Long libraryId) {
+        if (accounts.viewInfo(username) == null) {
+            accounts.create(new Personnel(10, name, surname, username, password, libraries.findById(libraryId)));
+            return true;
+        } else
+            return false;
+    }
+
+    public static boolean addLibrarian(String name, String surname, String username, String password, Long libraryId) {
+        if (accounts.viewInfo(username) == null) {
+            accounts.create(new Personnel(10, name, surname, username, password, libraries.findById(libraryId)));
             return true;
         } else
             return false;
@@ -89,7 +108,7 @@ public class MainService {
     public static boolean isUniqueUserName(String username) {
         if (accounts.viewInfo(username) == null)
             return true;
-        return false; 
+        return false;
     }
 
     public static List<Library> listLibraries() {
